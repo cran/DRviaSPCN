e98d94437a388865ba9c39f6f2fa4d38b4c95d02
Junwei Han <hanjunwei1981@163.com>
2025-01-09 11:20:06 +0000

    version 0.1.5

--- inst/doc/DRviaSPCN.R
+++ inst/doc/DRviaSPCN.R
@@ -2,15 +2,15 @@
 library(DRviaSPCN)
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  ### Download DRviaSPCNData package from GitHub
-#  
-#  library(devtools)
-#  install_github("hanjunwei-lab/DRviaSPCNData",force = TRUE)
-#  library(DRviaSPCNData)
-#  ### Get weighted-ES of subpathways
-#  DrugSPESCMatrix<-GetData("DrugSPESCMatrix")
-#  ### Get p-value of subpathways centrality score
-#  DrugSPPvalueMatrix<-GetData("DrugSPPvalueMatrix")
+# ### Download DRviaSPCNData package from GitHub
+# 
+# library(devtools)
+# install_github("hanjunwei-lab/DRviaSPCNData",force = TRUE)
+# library(DRviaSPCNData)
+# ### Get weighted-ES of subpathways
+# DrugSPESCMatrix<-GetData("DrugSPESCMatrix")
+# ### Get p-value of subpathways centrality score
+# DrugSPPvalueMatrix<-GetData("DrugSPPvalueMatrix")
 
 ## ----message=FALSE------------------------------------------------------------
 ###Load depend package
@@ -20,10 +20,10 @@ GEP<-GetExample('GEP')# Get the gene expression profile
 Slabel<-GetExample('Slabel')# Get the sample class label
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  ###Run the function
-#  CentralityScoreResult<-CalCentralityScore(ExpData=GEP,Label=Slabel,nperm = 1000)
-#  
-#  
+# ###Run the function
+# CentralityScoreResult<-CalCentralityScore(ExpData=GEP,Label=Slabel,nperm = 1000)
+# 
+# 
 
 ## ----echo=FALSE---------------------------------------------------------------
 ###Get the result of this function
@@ -36,12 +36,12 @@ CentralityScoreResult[1:10,c(1,3,5,6,7)]
 
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  
-#  ###Run the function
-#  Opdrugresult<-Optimaldrugs(ExpData=GEP,Label=Slabel,DrugSPESC=DrugSPESCMatrix,
-#                CentralityScore=CentralityScoreResult,nperm=1000,topcut=10,
-#                pcut=0.01,weight=FALSE)
-#  
+# 
+# ###Run the function
+# Opdrugresult<-Optimaldrugs(ExpData=GEP,Label=Slabel,DrugSPESC=DrugSPESCMatrix,
+#               CentralityScore=CentralityScoreResult,nperm=1000,topcut=10,
+#               pcut=0.01,weight=FALSE)
+# 
 
 ## ----echo=FALSE---------------------------------------------------------------
 ###Get the result of this function
@@ -58,18 +58,6 @@ library(igraph)
 plotSPW("00020_4")
 
 
-## ----echo=FALSE---------------------------------------------------------------
-###Get the result of this function
-sdf<-GetExample('methotrexate')
-
-## ----eval=FALSE---------------------------------------------------------------
-#  #Get the drug sdf data from DRviaSPCNData package
-#  sdf<-DRviaSPCNData::GetData("sdfSET")
-
-## ----results='hide',message=FALSE,fig.width=7,fig.height=5--------------------
-getMolecularFm(drugname ="methotrexate",sdfSET=sdf)
-
-
 ## ----message=FALSE,results='hide',fig.width=7,fig.height=5--------------------
 ###Load depend package
 library(GSVA)
@@ -84,19 +72,19 @@ Disease2SPheatmap(CentralityScore=CentralityScoreResult,ExpData=GEP,Label=Slabel
                    fontsize.row=9,fontsize.col=10)
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  ###Load depend package
-#  library(GSVA)
-#  library(pheatmap)
-#  ###Run the function
-#  Drug2SPheatmap(drugname="methotrexate_HL60_6_8.8e-06",
-#                DrugSPPvalue=DrugSPPvalueMatrix,ExpData=GEP,
-#                Label=Slabel,pcut=0.05,bk=c(-2,2),cluster.rows=FALSE,
-#                cluster.cols=FALSE,show.rownames=TRUE,
-#                show.colnames=FALSE,col=c("navy","firebrick3"),
-#                cell.width=NA,cell.height=NA,scale="row",
-#                fontsize=6,fontsize.row=9,fontsize.col=10)
-#  
-#  
+# ###Load depend package
+# library(GSVA)
+# library(pheatmap)
+# ###Run the function
+# Drug2SPheatmap(drugname="methotrexate_HL60_6_8.8e-06",
+#               DrugSPPvalue=DrugSPPvalueMatrix,ExpData=GEP,
+#               Label=Slabel,pcut=0.05,bk=c(-2,2),cluster.rows=FALSE,
+#               cluster.cols=FALSE,show.rownames=TRUE,
+#               show.colnames=FALSE,col=c("navy","firebrick3"),
+#               cell.width=NA,cell.height=NA,scale="row",
+#               fontsize=6,fontsize.row=9,fontsize.col=10)
+# 
+# 
 
 ## ----echo=FALSE---------------------------------------------------------------
 knitr::include_graphics("../inst/heat.png")
